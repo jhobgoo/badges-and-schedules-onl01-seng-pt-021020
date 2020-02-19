e6f29de37e1aded75c9c6cb4540aa50665f658c5
Joel Hobgood <jhobgoo@gmail.com>
2020-02-19 04:16:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ end
   
 def assign_rooms(name)
   array = []
-  room_assignments.each_with_index{ |name, room| room_assignments(name) }
+  array.each_with_index{ |name, room| room_assignments(name) }
 end
