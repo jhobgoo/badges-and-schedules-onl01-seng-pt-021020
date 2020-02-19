7674ca67d5755472969c8028b9e44e1a21779909
Joel Hobgood <jhobgoo@gmail.com>
2020-02-19 03:38:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |speaker|
+    return "Hello, my name is #{speaker}."
+  end
+end
+  
