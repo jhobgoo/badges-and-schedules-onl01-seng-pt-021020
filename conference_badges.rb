72266fa2dd9a9562238b8ccbd21043921fc66ad0
Joel Hobgood <jhobgoo@gmail.com>
2020-02-19 04:43:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,5 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each{|badge| puts |badge }
-  assign_rooms(attendees).each{ |room_assignments| puts |room_assignments|
+  assign_rooms(attendees).each{ |room_assignments| puts |room_assignments| }
 end
